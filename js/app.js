1d6a62c557982e6169dc5a97d1709dae3db01f83
Basirul Billah <basirulbillah@gmail.com>
2022-03-02 14:35:47 +0600

    correction to displayData

--- js/app.js
+++ js/app.js
@@ -1,37 +1,44 @@
 const loadData = () => {
-    const searchText = document.getElementById('search-field').value;
-    console.log(searchText);
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    console.log(url);
+    fetch(url)
         .then(res => res.json())
-        .then(data => displayData(data));
+        .then(data => displayData(data.data));
 }
 
 const displayData = data => {
-    // console.log(data);
     const searchDiv = document.getElementById('search-result');
     data.forEach(data => {
-        searchDiv.classList.add('card mb-3 p-2');
-        searchDiv.innerHTML = `
-        <div class="row g-0">
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card mb-3 p-2">
+            <div class="row g-0">
 
-            <!-- image -->
-            <div class="col-md-3" style="max-width: 540px;">
-                <img src="apple-iphone-13-mini.jpg" class="img-fluid rounded-start">
-            </div>
-
-            <!-- phone title -->
-            <div class="col-md-6">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <!-- image -->
+                <div class="col-md-3" style="max-width: 540px;">
+                    <img src="${data.image}" class="img-fluid rounded-start">
                 </div>
-            </div>
 
-            <!-- details button -->
-            <div class="col-md-3 align-items-center d-inline-flex flex-row-reverse p-2">
-                <button class="btn btn-warning">Show Details</button>
+                <!-- phone title -->
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.phone_name}</h5>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+
+                <!-- details button -->
+                <div class="col-md-3 align-items-center d-inline-flex flex-row-reverse p-2">
+                    <button class="btn btn-warning">Show Details</button>
+                </div>
             </div>
         </div>
         `
+        searchDiv.appendChild(div);
     })
 }
